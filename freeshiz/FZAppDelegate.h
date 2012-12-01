0064ed48053443f78e532cd911f96f3348f1bf86
Zac Bowling <zac@zacbowling.com>
2012-12-01 13:27:25 -0800

    Initial Commit

---
+++ freeshiz/FZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FZAppDelegate.h
+//  freeshiz
+//
+//  Created by Zac Bowling on 12/1/12.
+//  Copyright (c) 2012 freeshiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
